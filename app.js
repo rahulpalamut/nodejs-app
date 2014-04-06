6a22fe8532e475a1d864bb861c50508d6afcb8bf
rahulpalamut <rahulpalamut@gmail.com>
2014-04-06 00:39:41 -0700

    this could be cool

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app'));
 
 app.listen(port);
