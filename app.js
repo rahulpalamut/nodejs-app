f299175a9f3b3f13dc777bf9375a4e40ef2fb36e
rahulpalamut <rahulpalamut@gmail.com>
2014-04-06 11:16:50 -0700

    Halejelujayhhasdasdsa

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ app = express();
 
 
 app.configure(function(){
-    app.use('bower_components', express.static(__dirname+'/bower_components'));
-    app.use('fonts', express.static(__dirname+'/fonts'));
-    app.use('images', express.static(__dirname+'/images'));
-    app.use('views', express.static(__dirname+'/views'));
-    app.use('styles', express.static(__dirname+'/stles'));
-    app.use('scripts', express.static(__dirname+'/scripts'));
+    app.use('/bower_components/angular-leaflet/dist/', express.static(__dirname+'/bower_components/angular-leaflet/dist/'));
+    app.use('/bower_components/angular-collection/', express.static(__dirname+'/bower_components/angular-collection'));
+    app.use('/bower_components/bootstrap/dist/css/', express.static(__dirname+'/bower_components/bootstrap/dist/css/'));
+ app.use('/bower_components/bootstrap/dist/fonts/', express.static(__dirname+'/bower_components/bootstrap/dist/fonts/'));
+    
+    
+    app.use('/fonts/', express.static(__dirname+'/fonts/'));
+    app.use('/images/', express.static(__dirname+'/images/'));
+    app.use('/views', express.static(__dirname+'/views/'));
+    app.use('/styles/', express.static(__dirname+'/styles/'));
+    app.use('/scripts/', express.static(__dirname+'/scripts/'));
+    app.use('/bower_components/angular/', express.static(__dirname+'/bower_components/angular/'));
+    app.use('/bower_components/angular-cookies/', express.static(__dirname+'/bower_components/angular-cookies/'));
+    app.use('/bower_components/angular-resource/', express.static(__dirname+'/bower_components/angular-resource/'));
+    app.use('/bower_components/angular-sanitize/', express.static(__dirname+'/bower_components/angular-sanitize/'));
+    app.use('/bower_components/angular-route/', express.static(__dirname+'/bower_components/angular-route/'));
+
 });
 
 app.get('/', function(req, res){
