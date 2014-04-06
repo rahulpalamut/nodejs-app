cb9093984686b1b87763e2232fa00198e89f457a
rahulpalamut <rahulpalamut@gmail.com>
2014-04-05 17:51:22 -0700

    working statics

--- web.js
+++ web.js
@@ -1,49 +1,19 @@
 //web.js
-var express = require("express");
-// var logfmt = require("logfmt");
-var app = express();
-
-// app.use(logfmt.requestLogger());
-
-// app.get('/', function(req, res) {
-
-//     res.send('Hello World!');
-// });
-
-//var port = Number(process.env.PORT || 5000);
-// app.listen(port, function() {
-
-//    console.log("Listening on " + port);
-// });
 
 var port = Number(process.env.PORT || 5000);
-// var sys = require('sys'),
-//     http = require('http'),
-//     fs = require('fs'),
-//     index;
- 
-// fs.readFile('./index.html', function (err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     index = data;
-// });
-// console.log("Listening on " + port); 
-// http.createServer(function(request, response) {
-//     response.writeHeader(200, {"Content-Type": "text/html"});
-//     response.write(index);
-//     //response.close();
-// }).listen(port);
 
 var express = require('express'),
-    app = express.createServer();
+app = express();
+
 
 app.configure(function(){
-  app.use(express.static(__dirname + '/static'));
+    app.use('/assets/css', express.static(__dirname+'/assets/css'));
+    app.use('/assets/js', express.static(__dirname+'/assets/js'));
+    app.use('/xassets/screenshots', express.static(__dirname+'/assets/screenshots'));
+    app.use('/assets/images', express.static(__dirname+'/assets/images'));
 });
 
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
-
 app.listen(port);
