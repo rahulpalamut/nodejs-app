88475a35361346aca3157874918bafe305ce0971
rahulpalamut <rahulpalamut@gmail.com>
2014-04-05 22:17:40 -0700

    yoyo

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: grunt biuld
+web: grunt serve
---
+++ web.js
@@ -0,0 +1,20 @@
+//web.js
+
+var port = Number(process.env.PORT || 5000);
+
+var express = require('express'),
+app = express();
+
+
+app.configure(function(){
+    // app.use('/assets/css', express.static(__dirname+'/assets/css'));
+    // app.use('/assets/js', express.static(__dirname+'/assets/js'));
+    // app.use('/xassets/screenshots', express.static(__dirname+'/assets/screenshots'));
+    // app.use('/assets/images', express.static(__dirname+'/assets/images'));
+    app.use('/app', express.static( dirname+'/assets/css'));
+});
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+app.listen(port);
