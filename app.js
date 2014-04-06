5af5c9394d866b1f6318852cc71f14c3ee8bcfb9
rahulpalamut <rahulpalamut@gmail.com>
2014-04-06 14:26:02 -0700

    something

--- app.js
+++ app.js
@@ -159,155 +159,168 @@ mongoose.connect(uristring, function (err, res) {
     }
 });
 
-    app.configure(function(){
-	app.use('/bower_components/angular-leaflet/dist/', express.static(__dirname+'/bower_components/angular-leaflet/dist/'));
-	app.use('/bower_components/angular-collection/', express.static(__dirname+'/bower_components/angular-collection'));
-	app.use('/bower_components/bootstrap/dist/css/', express.static(__dirname+'/bower_components/bootstrap/dist/css/'));
-	app.use('/bower_components/bootstrap/dist/fonts/', express.static(__dirname+'/bower_components/bootstrap/dist/fonts/'));
-	
-	
-	app.use('/fonts/', express.static(__dirname+'/fonts/'));
-	app.use('/images/', express.static(__dirname+'/images/'));
-	app.use('/views', express.static(__dirname+'/views/'));
-	app.use('/styles/', express.static(__dirname+'/styles/'));
-	app.use('/scripts/', express.static(__dirname+'/scripts/'));
-	app.use('/bower_components/angular/', express.static(__dirname+'/bower_components/angular/'));
-	app.use('/bower_components/angular-cookies/', express.static(__dirname+'/bower_components/angular-cookies/'));
-	app.use('/bower_components/angular-resource/', express.static(__dirname+'/bower_components/angular-resource/'));
-	app.use('/bower_components/angular-sanitize/', express.static(__dirname+'/bower_components/angular-sanitize/'));
-	app.use('/bower_components/angular-route/', express.static(__dirname+'/bower_components/angular-route/'));
+app.configure(function(){
+    app.use('/bower_components/angular-leaflet/dist/', express.static(__dirname+'/bower_components/angular-leaflet/dist/'));
+    app.use('/bower_components/angular-collection/', express.static(__dirname+'/bower_components/angular-collection'));
+    app.use('/bower_components/bootstrap/dist/css/', express.static(__dirname+'/bower_components/bootstrap/dist/css/'));
+    app.use('/bower_components/bootstrap/dist/fonts/', express.static(__dirname+'/bower_components/bootstrap/dist/fonts/'));
+    
+    
+    app.use('/fonts/', express.static(__dirname+'/fonts/'));
+    app.use('/images/', express.static(__dirname+'/images/'));
+    app.use('/views', express.static(__dirname+'/views/'));
+    app.use('/styles/', express.static(__dirname+'/styles/'));
+    app.use('/scripts/', express.static(__dirname+'/scripts/'));
+    app.use('/bower_components/angular/', express.static(__dirname+'/bower_components/angular/'));
+    app.use('/bower_components/angular-cookies/', express.static(__dirname+'/bower_components/angular-cookies/'));
+    app.use('/bower_components/angular-resource/', express.static(__dirname+'/bower_components/angular-resource/'));
+    app.use('/bower_components/angular-sanitize/', express.static(__dirname+'/bower_components/angular-sanitize/'));
+    app.use('/bower_components/angular-route/', express.static(__dirname+'/bower_components/angular-route/'));
 
+});
+
+app.get('/send',function(req,res){
+
+    // These vars are your accountSid and authToken from twilio.com/user/account
+    ///send?to=2025961749&message=helloworld
+
+    var args = req.query;
+
+    client.messages.create({
+        body: args.message,
+        to: args.to,
+        from: "+2406502344",
+    }, function(err, message) {
+        if (err) {
+	    console.log(err);
+	    res.send(err);
+
+        }
+        console.log(message.sid);
+        res.send(message.sid);
     });
+});
 
-    app.get('/send',function(req,res){
+app.get('/recv',function(req,res){
+    var message = req.body.Body;
+    var from = req.body.From;
+    var tokens = message.split(" ");
+    var location = tokens[0].toLowerCase();
+    var food = tokens[1].toLowerCase();
+    var args = req.query;
 
-	// These vars are your accountSid and authToken from twilio.com/user/account
-	///send?to=2025961749&message=helloworld
-
-	var args = req.query;
-
-	client.messages.create({
-            body: args.message,
-            to: args.to,
-            from: "+2406502344",
-	}, function(err, message) {
-            if (err) {
-		console.log(err);
-		res.send(err);
-
-            }
-            console.log(message.sid);
-            res.send(message.sid);
-	});
+    client.messages.create({
+        body: food,
+        to: from,
+        from: "+2406502344",
+    }, function(err, message) {
+        if (err) {
+	    console.log(err);
+	    res.send(err);
+        }
+        //console.log(message.sid);
+        res.send(message.sid);
     });
+    //getClosest(location,food);
+});
 
-    app.get('/recv',function(req,res){
-	var message = req.body.Body;
-	var from = req.body.From;
-	var tokens = message.split(" ");
-	var location = tokens[0].toLowerCase();
-	var food = tokens[1].toLowerCase();
+app.get('/text',function(req,res){
 
-	getClosest(location,food);
-    });
+    var url = require('url');
+    var query = url.parse(request.url,true).query;
 
-    app.get('/text',function(req,res){
+    getClosest(query.location,query.food);
+});
 
-	var url = require('url');
-	var query = url.parse(request.url,true).query;
+app.get('/joe', function(req, res){
+    //sending maybe food, maybe location
+    //returning list of dining halls 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    var message = req.query.message.toLowerCase();
+    message = message.replace(/^\s+|\s+$/g,'')
+    console.log(message)
 
-	getClosest(query.location,query.food);
-    });
+    Meal.find({'fooditem':message, 'meals': whichmeal()}, 'dininghalls', function(err, dhs){
+	if (err) return handleError(err);
+  	//console.log('%s %s is a %s.', person.name.first, person.name.last, person.occupation)
+  	console.log(dhs)
+        res.send(dhs);
+    })
+    /*
+      Meal.find(function (err, food) {
+      if (err) return console.error(err);
+      console.log(food)
+      res.send(food);
+      })
+    */		  //console.log(req.route.params);
+})
 
-    app.get('/joe', function(req, res){
-	//sending maybe food, maybe location
-	//returning list of dining halls 
-        res.setHeader("Access-Control-Allow-Origin", "*");
-	var message = req.query.message.toLowerCase();
-	message = message.replace(/^\s+|\s+$/g,'')
-	console.log(message)
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
 
-	Meal.find({'fooditem':message, 'meals': whichmeal()}, 'dininghalls', function(err, dhs){
-	    if (err) return handleError(err);
-  	    //console.log('%s %s is a %s.', person.name.first, person.name.last, person.occupation)
-  	    console.log(dhs)
-            res.send(dhs);
-	})
-/*
-	Meal.find(function (err, food) {
-	    if (err) return console.error(err);
-	    console.log(food)
-	    res.send(food);
-	})
-*/		  //console.log(req.route.params);
-		 })
+app.get('/scrape', function(req, res){
+    res.send('scraped') 
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=30&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'eight')
 
-    app.get('/', function(req, res){
-	res.sendfile(__dirname + '/index.html');
-    });
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=30&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'eight')
 
-    app.get('/scrape', function(req, res){
-       res.send('scraped') 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=30&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'eight')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=30&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'eight')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=30&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'eight')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=5&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'cowell')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=30&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'eight')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=5&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'cowell')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=5&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'cowell')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=5&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'cowell')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=5&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'cowell')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=20&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'crown')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=5&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'cowell')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=20&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'crown')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=20&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'crown')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=20&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'crown')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=20&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'crown')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=25&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'porter')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=20&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'crown')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=25&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'porter')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=25&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'porter')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=25&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'porter')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=25&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'porter')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=40&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'nine')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=25&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'porter')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=40&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'nine')
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=40&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Breakfast+++served+until+11am&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'breakfast', 'nine')
+    httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=40&dtdate=' 
+	    + date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
+	    '&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'nine')
+});
 
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=40&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Lunch served until 2pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'lunch', 'nine')
-
-httpGet('http://www.kimonolabs.com/api/ce6dd1oc?&?locationNum=40&dtdate=' 
-	+ date.getMonth() + '%2F' + date.getDate() + '%2F' + date.getYear() + 
-	'&Dinner+served+until+8pm&apikey=2323b50a71ade7d336c82c9f9dd5c072', 'dinner', 'nine')
-    });
-
-    app.listen(port);
+app.listen(port);
 
