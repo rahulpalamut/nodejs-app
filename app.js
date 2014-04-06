5d714d5aadb4521a7125b06209b5e221317bd590
rahulpalamut <rahulpalamut@gmail.com>
2014-04-06 00:46:31 -0700

    this

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require('express');
 var port = process.env.PORT || 5000;
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port);
